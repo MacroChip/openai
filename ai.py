721e38bd941b0ca2b0d6a978d833d6a67ed7fffa
Chip Thien <chip@example.com>
2017-02-19 12:53:54 -0600

    Must send in vector of actions, not just one action. Also move constants to global scope.

--- ai.py
+++ ai.py
@@ -2,23 +2,24 @@ import gym
 import universe  # register the universe environments
 import random
 
+FORWARD = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
+RIGHT = [('KeyEvent', 'ArrowRight', True), ('KeyEvent', 'ArrowLeft', False)]
+LEFT = [('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
+
 def main():
     env = gym.make('flashgames.DuskDrive-v0')
     env.configure(remotes=1)  # automatically creates a local docker container
     observation_n = env.reset()
 
-    FORWARD = [('KeyEvent', 'ArrowUp', True), ('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', False)]
-    RIGHT = [('KeyEvent', 'ArrowRight', True), ('KeyEvent', 'ArrowLeft', False)]
-    LEFT = [('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
     current_action = FORWARD
     action_n = [FORWARD for ob in observation_n]
-    observation_n, reward_n, done_n, info = env.step(current_action)
+    observation_n, reward_n, done_n, info = env.step(action_n)
     env.render()
 
     while True:
         current_action = determine_action(current_action, reward_n)
         action_n = [current_action for ob in observation_n]
-        observation_n, reward_n, done_n, info = env.step(current_action)
+        observation_n, reward_n, done_n, info = env.step(action_n)
         env.render()
 
 
